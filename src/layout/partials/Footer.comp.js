0aaf0c9cf18b794415963ca0f5a82bb63e782d98
Dinesh Parekh <dinesh21045@gmail.com>
2022-03-02 23:52:03 +0530

    App set up. added

---
+++ src/layout/partials/Footer.comp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center copy-right">
+      &copy; ProAdMan all right reserved - 2020.
+    </div>
+  );
+};
